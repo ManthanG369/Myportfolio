da7a975fa057c3a9679e2e0da9734263ae9be429
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2022-01-07 02:29:27 +0530

    skills section

--- Assets/script/main.js
+++ Assets/script/main.js
@@ -29,3 +29,26 @@ function linkAction(){
 for(let i=0;i<navLink.length;i++){
         navLink[i].addEventListener('click',linkAction)
 }
+
+
+/* ==============Accordition skills================ */
+
+var skillsContent=document.getElementsByClassName("skills__content");
+var skillsHeader=document.querySelectorAll('.skills__header')
+
+
+function toggleSkills(){
+        let itemclass=this.parentNode.className
+
+        for(let i=0;i<skillsContent.length;i++){
+                skillsContent[i].className='skills__content skills__close'
+        }
+
+        if(itemclass==='skills__content skills__close'){
+                this.parentNode.className='skills__content skills__open'
+        }
+}
+
+skillsHeader.forEach((el)=>{
+        el.addEventListener('click',toggleSkills)
+})
