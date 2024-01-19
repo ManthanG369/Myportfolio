7c2ce1ef72e1abd0e0282aba42ecaf4099e9253a
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-01-19 15:12:01 +0530

    Added about section , social ID, app wrap

---
+++ frontend_react/src/components/SocialMedia.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { socialID } from "../constants";
+
+const SocialMedia = () => {
+  return (
+    <div className="app__social">
+      {socialID.map((item) => (
+        <div key={item}>
+          <a href={item.iconUrl} target="_blank" rel="noopener noreferrer">
+            {item.icon}
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SocialMedia;
---
+++ frontend_react/src/constants/social.js
@@ -0,0 +1,17 @@
+import { BsTwitter, BsInstagram } from "react-icons/bs";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
+export default [
+  {
+    icon: <FaGithub />,
+    iconUrl: "https://github.com/ManthanG369",
+  },
+  {
+    icon: <FaLinkedin />,
+    iconUrl: "https://www.linkedin.com/in/manthan-gaikwad/",
+  },
+  {
+    icon: <BsTwitter />,
+    iconUrl: "https://twitter.com/Manty369",
+  },
+];
