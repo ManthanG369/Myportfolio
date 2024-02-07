50bcc43a8072d752e00b83a148a1cc258f126632
Manthan Gaikwad <87421766+ManthanG369@users.noreply.github.com>
2024-02-07 21:25:49 +0530

    added skills and footer section

--- frontend_react/src/wrapper/MotionWrap.js
+++ frontend_react/src/wrapper/MotionWrap.js
@@ -5,8 +5,8 @@ const MotionWrap = (Component, classNames) =>
   function HOC() {
     return (
       <motion.div
-        whileInView={{ y: [100, 50, 0], opacity: [0, 0, 1] }}
-        transition={{ duration: 0.5 }}
+        whileInView={{ y: [200, 50, 0], opacity: [0, 0, 1] }}
+        transition={{ duration: 0.8 }}
         className={`${classNames} app__flex`}
       >
         <Component />
